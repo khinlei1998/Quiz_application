075bedeb0cbae26e2e6aea8ed76d7e757942d9dd
Yati <perthkingdom2001@gmail.com>
2020-08-15 22:51:18 +0630

    second push

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-const express= require('express')
-const serveStatic =require('serve-static')
-const path=require('path')
+const express= require('express');
+// const serveStatic =require('serve-static')
+// const path=require('path')
 
-const app=express()
 
-app.use('/',serveStatic(path.join(_dirname,'/dist')))
-const port=proecess.env.PORT || 8080
+
+
+const port=proecess.env.PORT || 8080;
+ const app=express();
+  app.use(express.static(__dirname + "/dist/"));
+  app.get(/.*/, function(req, res){
+      res.sendfile(__dirname + "/dist/index.html"); 
+  });
 app.listen(port)
-console.log('Listening on port:' +port)
+console.log("server started ...")
